e1ed6efcc2e1c9d2a45366f9b32eaecc13494ad2
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2026-03-14 20:03:52 +0000

    fix: dark mode selection color blueish #384659

--- src/Forms/ExistingSessionsVisuals.cs
+++ src/Forms/ExistingSessionsVisuals.cs
@@ -302,7 +302,7 @@ internal class ExistingSessionsVisuals
                 Padding = new Padding(4, 4, 4, 4),
                 BackColor = Application.IsDarkModeEnabled ? Color.FromArgb(0x11, 0x11, 0x11) : SystemColors.Window,
                 ForeColor = Application.IsDarkModeEnabled ? Color.FromArgb(0xCC, 0xCC, 0xCC) : SystemColors.ControlText,
-                SelectionBackColor = Application.IsDarkModeEnabled ? Color.FromArgb(0x38, 0x38, 0x38) : Color.FromArgb(200, 220, 245),
+                SelectionBackColor = Application.IsDarkModeEnabled ? Color.FromArgb(0x38, 0x46, 0x59) : Color.FromArgb(200, 220, 245),
                 SelectionForeColor = Application.IsDarkModeEnabled ? Color.White : Color.Black
             },
             EnableHeadersVisualStyles = false,
